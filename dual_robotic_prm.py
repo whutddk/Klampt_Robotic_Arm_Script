56ae62c43c2ff2218a46d4e437d723aeffd9d648
whutddkUbuntu16 <295054118@whut.edu.cn>
2018-12-21 20:28:00 +0800

    RobotCollideRobot API

--- dual_robotic_prm.py
+++ dual_robotic_prm.py
@@ -23,9 +23,20 @@ def load_Pose():
 	pass
 
 
+def robotCollideRobot():
+	collisionTest = WorldCollider(world)
+	for i in collisionTest.robots[0]:
+		for j in collisionTest.robots[1]:
+			if j < 0: continue
+			if i < 0: continue
+			if collisionTest.geomList[j][1].collides(collisionTest.geomList[i][1]):
+				return True
+	return False
+
+
 
 if __name__ == "__main__":
-	load_Pose()
+	# load_Pose()
 	world = WorldModel()
 
 	res = world.readFile('./anno_check.xml')
@@ -34,28 +45,37 @@ if __name__ == "__main__":
 			
 	vis.add("world",world)
 
-	robot = world.robot(0)
+	prmRobot = world.robot(0)
+	ctlRobot = world.robot(1)
 	vis.show()
 	collisionTest = WorldCollider(world)
 	
-	robotPose = RobotPoser(robot)
+	prmRobotPose = RobotPoser(prmRobot)
+	ctlRobotPose = RobotPoser(ctlRobot)
 
 
+	prmRobotPose.set([0,pi/2,-pi/2, 0,   0, pi/2,0])
+	
 
-	robotPose.set([0,pi/2,-pi/2,0,0,pi/2,0])
-	collisionTest = WorldCollider(world)
-
-
-
-	for i in range(0,453):
-		Pose[i].insert(0,0)
-		# realPose.append(Pose[i])
-		print (Pose[i])
-		robotPose.set(Pose[i])
+	for k in range (0,50):
+		ctlRobotPose.set([0,-pi/100 * k + pi/2,   -pi/2    ,pi/2, 0, pi/2,0])
 		collisionTest = WorldCollider(world)
-		time.sleep(1)
-		vis.show()
+		time.sleep(0.1)
+	# for l in collisionTest.geomList[1]:
+	# 	print l
+		
+		if (robotCollideRobot()):
+			print "collision!"
 
+
+		
+
+
+
+	while(1):
+		time.sleep(0.1)
+		vis.show()
+		pass
 
 # set ctlRobot Random Pose
 
