254294bb1a2518e11501cfc36ea2d936d9ff1075
Ruige_Lee <295054118@qq.com>
2019-01-26 20:48:45 +0800

    生成文件按100ms*3x配置

---
+++ result/create_Edge_3m100ms/create_edge.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+# @File Name: create_edge.py
+# @File Path: M:\MAS2\Robotic Arm\Klampt_Robotic_Arm_Script\result\create_Edge_3m50ms\create_edge.py
+# @Author: Ruige_Lee
+# @Date:   2018-12-29 14:36:47
+# @Last Modified by:   Ruige_Lee
+# @Last Modified time: 2019-01-26 20:47:28
+# @Email: 295054118@whut.edu.cn"
+
+from klampt import *
+from klampt.model.collide import *
+import sys
+import time
+from klampt.sim import *
+from klampt import vis
+
+import json
+
+import random
+
+################################################
+ 
+
+_PI_ = 3.14159 
+
+
+jointList = []
+edgeIndexList = []
+edgeIndex = []
+
+def load_data():
+	global edgeIndex
+	global jointList
+
+	with open('./jointList.json','r') as jointListFile:
+			
+		data = jointListFile.read()
+		jointList = json.loads(data)
+	
+	with open('./edgeIndex.json','r') as edgeIndexFile:
+			
+		data = edgeIndexFile.read()
+		edgeIndex = json.loads(data)
+
+
+
+def save_jointList():
+	global jointList
+	with open('./jointList.json','w') as jointListFile:
+		
+		data = json.dumps(jointList)
+		jointListFile.write(data)
+	pass
+	
+
+def save_edgeIndex():
+	global edgeIndex
+	with open('./edgeIndex.json','w') as edgeIndexFile:
+		
+		data = json.dumps(edgeIndex)
+		edgeIndexFile.write(data)
+	pass
+
+
+
+
+def robotSelfCollideCheck():
+	collisionTest = WorldCollider(world)
+	# for i in collisionTest.robots[0]:
+	for i in range(0,8):
+		for j in range (0,8):
+			if ( ((i+1) == j) or ((i-1) == j) or ( i==j ) ):
+				continue
+			
+			if collisionTest.geomList[j][1].collides(collisionTest.geomList[i][1]):
+	 			return True
+	return False
+
+
+def prmRobotRandom():
+	axisSave = [		
+		random.randint(-3142 , 3142)/1000.,
+		random.randint(-2007 , 2007)/1000.,
+		random.randint(-698 , 3840)/1000.,
+		random.randint(-3142 , 3142)/1000.,
+		random.randint(-262 , 3403)/1000.,
+		random.randint(-3142 , 3142)/1000.
+	]
+	#print axisSave
+	axis = axisSave[:]
+	axis.insert(0,0)
+
+	prmRobotPose.set(axis)
+
+	return axisSave
+
+def edge_constraint(jointSet):
+	global jointList
+	global edgeIndex
+	global edgeNum
+
+	jointLenth = len(jointList)
+
+	save = False
+
+	for preIndex in range(0,jointLenth):
+	# for prePose in poseList:
+
+		if ( ( abs(jointList[preIndex][0] - jointSet[0]) < (0.417 / 180 * _PI_ *3 * 100 / 10  ) ) and
+			( abs(jointList[preIndex][1] - jointSet[1]) < (0.183 / 180 * _PI_ * 3 * 100 / 10 ) ) and
+			( abs(jointList[preIndex][2] - jointSet[2]) < (0.25 / 180 * _PI_ *3 * 100 / 10  ) ) and
+			( abs(jointList[preIndex][3] - jointSet[3]) < (0.2 / 180 * _PI_ * 3 * 100 / 10 ) ) and
+			( abs(jointList[preIndex][4] - jointSet[4]) < (0.2 / 180 * _PI_ * 3 * 100 / 10 ) ) and
+			( abs(jointList[preIndex][5] - jointSet[5]) < (0.543 / 180 * _PI_ * 3 * 100 / 10 ) ) 		and
+			
+			( abs(jointList[preIndex][0] - jointSet[0]) > (0.417 / 180 * _PI_ * 0.25   ) ) and
+			( abs(jointList[preIndex][1] - jointSet[1]) > (0.183 / 180 * _PI_* 0.25 ) ) and
+			( abs(jointList[preIndex][2] - jointSet[2]) > (0.25 / 180 * _PI_ * 0.25  ) ) and
+			( abs(jointList[preIndex][3] - jointSet[3]) > (0.2 / 180 * _PI_ * 0.25 ) ) and
+			( abs(jointList[preIndex][4] - jointSet[4]) > (0.2 / 180 * _PI_* 0.25 ) ) and
+			( abs(jointList[preIndex][5] - jointSet[5]) > (0.543 / 180 * _PI_* 0.25 ) ) ):
+			
+			
+			save = True
+			edgeNum = edgeNum + 1
+			edge = [preIndex,jointLenth]
+			edgeIndex.append(edge)
+			
+
+			save_edgeIndex()
+			
+			print edge
+
+	if ( save == True ):
+		jointList.append(jointSet)
+		save_jointList()
+		print len(edgeIndex)
+
+
+###############################
+
+
+
+if __name__ == "__main__":
+
+
+	world = WorldModel()
+
+	res = world.readFile('./anno_check.xml')
+	if not res:
+		raise RuntimeError("Unable to load model ") 
+	del res
+
+	prmRobot = world.robot(0)
+
+	#vis.add("world",world)
+	#vis.show()
+
+
+	collisionTest = WorldCollider(world)
+	
+	prmRobotPose = RobotPoser(prmRobot)
+
+
+	load_data()
+
+	edgeNum = len(edgeIndex)
+	print edgeNum
+
+
+	while(edgeNum < 100000):
+	# while( len(jointList)<5000 ):
+		# time.sleep(0.1)
+		jointSet = prmRobotRandom()
+		if ( False == robotSelfCollideCheck() ):
+			
+			edge_constraint(jointSet)
+
+
+
+
+
+
+
