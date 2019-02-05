6e1eeecdde3870d03b19c267e99c154f24e92ba1
WUT_DDK_SFB <295054118@qq.com>
2019-02-05 21:00:26 +0800

    开始准备筛选edge

---
+++ edgeIndexRebuild/edgeIndexRebuild.py
@@ -0,0 +1,84 @@
+
+# @File Name: edgeIndexRebuild.py
+# @File Path: K:\work\MAS2\Klampt_Robotic_Arm_Script\edgeIndexRebuild\edgeIndexRebuild.py
+# @Author: 29505
+# @Date:   2019-02-05 20:38:20
+# @Last Modified by:   29505
+# @Last Modified time: 2019-02-05 20:58:47
+# @Email: 295054118@whut.edu.cn
+
+
+
+
+
+
+# dry run
+# 打开jointList 打开edgeIndex 
+
+# for edgeIndex until aim
+
+#	resave edgeIndex
+#	search maxJointList rebuild
+
+
+
+# heat run
+# 打开jointList 打开edgeIndex 打开heat文件
+
+# heat 生成二维，对一个维度进行排序
+# 筛选出新的edgeIndex
+# 查找joint，重新生成edge序号
+
+
+jointList = []
+edgeIndex = []
+edgeHeat = []
+
+newJointList = []
+newEdgeIndex = []
+
+
+def load_data():
+	global edgeIndex
+	global jointList
+	global edgeHeat
+
+	with open('./edgeHeat.json','r') as edgeHeatFile:
+		data = edgeHeatFile.read()
+		edgeHeat = json.loads(data)
+
+	with open('./jointList.json','r') as jointListFile:
+			
+		data = jointListFile.read()
+		jointList = json.loads(data)
+	
+	with open('./edgeIndex.json','r') as edgeIndexFile:
+			
+		data = edgeIndexFile.read()
+		edgeIndex = json.loads(data)
+
+	pass
+
+
+
+def save_jointList():
+	global newJointList
+	with open('./newJointList.json','w') as newJointListFile:
+		
+		data = json.dumps(newJointList)
+		newJointListFile.write(data)
+	pass
+	
+
+def save_edgeIndex():
+	global newEdgeIndex
+	with open('./newEdgeIndex.json','w') as newEdgeIndexFile:
+		
+		data = json.dumps(newEdgeIndex)
+		newEdgeIndexFile.write(data)
+	pass
+
+
+def heat_rebuild():
+	pass
+
