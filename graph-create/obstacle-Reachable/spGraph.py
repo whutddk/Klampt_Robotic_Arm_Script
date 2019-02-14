0d14046ca2ec801bfc25c217da97ec31a69d0eff
WUT_DDK_SFB <295054118@qq.com>
2019-02-14 22:28:01 +0800

    修正SP graph

--- graph-create/obstacle-Reachable/spGraph.py
+++ graph-create/obstacle-Reachable/spGraph.py
@@ -1,9 +1,9 @@
-# @File Name: spMain.py
-# @File Path: K:\work\MAS2\Klampt_Robotic_Arm_Script\gridModelEncode\sphericalCoordinate\spMain.py
+# @File Name: spGraph.py
+# @File Path: K:\work\MAS2\PRM_robotic_arm\Klampt_Robotic_Arm_Script\graph-create\obstacle-Reachable\spGraph.py
 # @Author: 29505
 # @Date:   2019-02-07 09:33:58
 # @Last Modified by:   29505
-# @Last Modified time: 2019-02-09 11:45:53
+# @Last Modified time: 2019-02-14 22:04:32
 # @Email: 295054118@whut.edu.cn
 
 from klampt import *
@@ -18,11 +18,7 @@ import json
 import random
 import math
 
-Pose = []
-edge = []
-edgeIndex = []
 
-trueTable = []
 
 def make_testing_mesh(world):
 	"""automatically create a mesh test grid
@@ -31,25 +27,25 @@ def make_testing_mesh(world):
 		for y in range (0,32):
 			for x in range (0,16):
 
-				radius = 0.120 + 0.023*x
-				theta = -1.3197 + 0.0825*y
-				fieta = -1.57 + 0.098*z
+				# radius = 0.120 + 0.023*x
+				# theta = -1.3197 + 0.0825*y
+				# fieta = -1.57 + 0.098*z
 
-				XTest = (radius + 0.023) * math.cos(theta) * math.cos(fieta)
-				ZMinTest = (radius + 0.023) * math.sin(fieta+0.098)+0.264
-				ZMaxTest = (radius ) * math.sin(fieta)+0.264
+				# XTest = (radius + 0.023) * math.cos(theta) * math.cos(fieta)
+				# ZMinTest = (radius + 0.023) * math.sin(fieta+0.098)+0.264
+				# ZMaxTest = (radius ) * math.sin(fieta)+0.264
 
-				if ( XTest < 0.120): 
+				# if ( XTest < 0.120): 
 
-					continue
-				if (ZMinTest < 0):
-					continue
-				if (ZMaxTest > 0.640):
-					continue
+				# 	continue
+				# if (ZMinTest < 0):
+				# 	continue
+				# if (ZMaxTest > 0.640):
+				# 	continue
 
 				grid = Geometry3D()
 
-				grid.loadFile('./spModel/trapezoid'+str(x)+'_'+str(y)+'_'+str(z)+'.off')
+				grid.loadFile('../../gridModelEncode/sphericalCoordinate/spModel/trapezoid'+str(x)+'_'+str(y)+'_'+str(z)+'.off')
 
 				grid.transform([1,0,0,0,1,0,0,0,1],[0,0,0])			
 
@@ -59,114 +55,6 @@ def make_testing_mesh(world):
 				Mesh.appearance().setColor(0.3,0.1,0.1,0.1)
 	return 
 
-def load_Pose():
-	global Pose
-	global edgeIndex
-	global edge
-
-	with open('../../result/create_Edge_3m250ms/jointList.json','r') as poseFile:
-		data = poseFile.read()
-		Pose = json.loads(data)
-		print (Pose)
-	pass
-
-def load_edge():
-	global Pose
-	global edgeIndex
-	global edge
-
-	with open('F:/klampt/250msx3grid-sp/edge.json','r') as edgeFile:
-		data = edgeFile.read()
-		edge = json.loads(data)
-		#print edge
-
-def load_Index():
-
-	global Pose
-	global edgeIndex
-	global edge
-
-	with open('../../result/create_Edge_3m250ms/HeatCut/4096/edgeIndex.json','r') as edgeIndexFile:
-		data = edgeIndexFile.read()
-		edgeIndex = json.loads(data)
-		
-		print (edgeIndex)
-
-def store_Edge():
-	global Pose
-	global edgeIndex
-	global edge
-
-	with open('F:/klampt/250msx3grid-sp/edge.json','w') as edgeFile:
-		data = json.dumps(edge)
-		edgeFile.write(data)
-	pass
-
-
-
-def create_Edge(Index):
-
-	global Pose
-	global edgeIndex
-	global edge
-
-	print ("now Create Edge:")
-	print (Index)
-
-	i = edgeIndex[Index][0]
-	j = edgeIndex[Index][1]
-
-	shoulderStart = Pose[j][0]
-	armStart = Pose[j][1]
-	elbowStart = Pose[j][2]
-	wristStart = Pose[j][3]
-	fingerStart = Pose[j][4]
-	toolStart = Pose[j][5]
-
-	shoulderEnd = Pose[i][0]
-	armEnd = Pose[i][1]
-	elbowEnd = Pose[i][2]
-	wristEnd = Pose[i][3]
-	fingerEnd = Pose[i][4]
-	toolEnd = Pose[i][5]
-
-	shoulderDis = (shoulderEnd - shoulderStart) / 100
-	armDis = (armEnd - armStart) / 100
-	elbowDis = ( elbowEnd - elbowStart ) / 100
-	wristDis = ( wristEnd - wristStart ) / 100
-	fingerDis = ( fingerEnd - fingerStart ) / 100
-	toolDis = ( toolEnd - toolStart ) / 100
-
-	oneEdge = [0 for m in range(0,16384)]
-
-	for k in range (0,101):
-		time.sleep(0.01)
-		robotPose.set([0,
-			(shoulderStart + shoulderDis*k),
-			(armStart + armDis*k),
-			(elbowStart + elbowDis*k),
-			(wristStart + wristDis*k),
-			(fingerStart + fingerDis*k),
-			toolStart + toolDis*k,
-			0]
-			)
-		collisionTest = WorldCollider(world)
-
-		cnt = 0;
-		for p,q in collisionTest.robotTerrainCollisions(0):
-			result = q.getName()
-			
-			#print q.getName()
-			x = int(result[5:8])
-			y = int(result [9:12])
-			z = int(result[13:16])
-			oneEdge[1024*x+32*y+z] = 1
-			cnt = cnt + 1;
-		print ("cnt in this frame")
-		print (cnt)
-	edge.append(oneEdge)
-	store_Edge()
-	pass
 
 if __name__ == "__main__":
 	
@@ -176,35 +64,23 @@ if __name__ == "__main__":
 	if not res:
 		raise RuntimeError("Unable to load model ") 
 			
-	load_Pose()
-	load_Index()
-	load_edge()
-
 	make_testing_mesh(world)
 				
-	
-	#sim = Simulator(world)
+
 	robot = world.robot(0)
 
-	# vis.add("world",world)
-	# vis.show()
+	vis.add("world",world)
+	vis.show()
 
 	collisionTest = WorldCollider(world)
 	
 	robotPose = RobotPoser(robot)
 	
-	#print robotPose.get()
-	while(len(edge) < 4096):
-		create_Edge(len(edge))
 
+	while(1):
+		time.sleep(0.1)
+		vis.shown()
+		pass
 
-
-
-	#while(1):
-		#time.sleep(0.1)
-		#vis.shown()
-		#pass
-
-			#pass
 			
 
