10b24ede0ef1f2ff385191942491bab62c9820f3
Ruige_Lee <295054118@qq.com>
2019-02-18 10:02:31 +0800

    comfirm obstacle

--- graph-create/obstacle-Reachable/ObGraph.py
+++ graph-create/obstacle-Reachable/ObGraph.py
@@ -1,5 +1,14 @@
-# @File Name: CaGraph.py
-# @File Path: K:\work\MAS2\PRM_robotic_arm\Klampt_Robotic_Arm_Script\graph-create\obstacle-Reachable\CaGraph.py
+# -*- coding: utf-8 -*-
+# @File Name: ObGraph.py
+# @File Path: M:\MAS2\PRM_Robotic_Arm\Klampt_Robotic_Arm_Script\graph-create\obstacle-Reachable\ObGraph.py
+# @Author: Ruige_Lee
+# @Date:   2019-02-18 09:44:06
+# @Last Modified by:   Ruige_Lee
+# @Last Modified time: 2019-02-18 10:02:02
+# @Email: 295054118@whut.edu.cn"
+
+# @File Name: ObGraph.py
+# @File Path: M:\MAS2\PRM_Robotic_Arm\Klampt_Robotic_Arm_Script\graph-create\obstacle-Reachable\ObGraph.py
 # @Author: 29505
 # @Date:   2018-12-30 09:59:45
 # @Last Modified by:   29505
@@ -18,23 +27,19 @@ import json
 import random
 
 
-def make_testing_mesh(world):
-	"""automatically create a mesh test grid
-	"""
+def make_obstacle(world):
 
-	for z in range(0,32):
-		for y in range (0,32):
-			for x in range (0,16):
-				grid = Geometry3D()
+	grid = Geometry3D()
 
-				grid.loadFile("../../terrains/cube.off")
+	grid.loadFile("../../terrains/cube.off")
 
-				grid.transform([0.023,0,0,  0,0.030,0,  0,0,0.020],[0.023*x + 0.120,0.030*y-0.480,0.020*z])			
+	grid.transform([0.368,0,0,  0,0.960,0,  0,0,0.640],[0.120,-0.480,0])			
 
-				Mesh = world.makeTerrain("Grid," + "%3d"%x + "," + "%3d"%y + "," + "%3d"%z)
+	Mesh = world.makeTerrain("obstacleArea")
 
-				Mesh.geometry().set(grid)
-				Mesh.appearance().setColor(0.1,0.1,0.2,0.1)
+	Mesh.geometry().set(grid)
+	Mesh.appearance().setColor(0.1,0.1,0.2,0.1)
+
 	return 
 
 
@@ -47,7 +52,7 @@ if __name__ == "__main__":
 		raise RuntimeError("Unable to load model ") 
 			
 
-	make_testing_mesh(world)
+	make_obstacle(world)
 				
 
 	robot = world.robot(0)
