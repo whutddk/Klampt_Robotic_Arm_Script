f553b7b7f798fdcd9422621e1bc6c89827bf1eaf
WUT_DDK_SFB <295054118@qq.com>
2019-04-18 17:31:21 +0800

    优化串口协议

--- hostSystem/dual_robotic_host.py
+++ hostSystem/dual_robotic_host.py
@@ -1,6 +1,14 @@
+# @File Name: dual_robotic_host.py
+# @File Path: K:\work\MAS2\PRM_robotic_arm\Klampt_Robotic_Arm_Script\hostSystem\dual_robotic_host.py
+# @Author: 29505
+# @Date:   2019-04-18 16:53:15
+# @Last Modified by:   29505
+# @Last Modified time: 2019-04-18 17:30:48
+# @Email: 295054118@whut.edu.cn
+# @page: https://whutddk.github.io/
 # -*- coding: utf-8 -*-
 # @File Name: dual_robotic_host.py
-# @File Path: M:\MAS2\PRM_Robotic_Arm\Klampt_Robotic_Arm_Script\hostSystem\dual_robotic_host.py
+# @File Path: K:\work\MAS2\PRM_robotic_arm\Klampt_Robotic_Arm_Script\hostSystem\dual_robotic_host.py
 # @Author: Ruige_Lee
 # @Date:   2019-04-17 18:54:16
 # @Last Modified by:   Ruige_Lee
@@ -24,10 +32,10 @@ _STEPNUM_ = 25
 
 
 
-class freecars():
+class freeKlampt():
 	def __init__(self):
 		self.recBuf = []
-		self.recChk = 0
+		self.recChk = 1
 		self.axisA = [0,0,0,0,0,0]
 		self.axisB = [0,0,0,0,0,0]
 
@@ -39,59 +47,74 @@ class freecars():
 		self.rob2 = [0]
 
 	def send_powerUp(self):
-		self.ser.write()
+		self.ser.write(255)
+		self.ser.write(85)
+		self.ser.write(170)
+		self.ser.write(119)
+		self.ser.write(0)
+		self.ser.write(11)
+		self.ser.write(128)
+
 
 	def send_powerDown(self):
-		self.ser.write()	
+		self.ser.write(255)
+		self.ser.write(85)
+		self.ser.write(170)
+		self.ser.write(119)
+		self.ser.write(0)
+		self.ser.write(12)
+		self.ser.write(129)
 
 	def send_enable(self):
-		self.ser.write()
+		self.ser.write(255)
+		self.ser.write(85)
+		self.ser.write(170)
+		self.ser.write(119)
+		self.ser.write(0)
+		self.ser.write(101)
+		self.ser.write(218)
 
 	def send_disable(self):
-		self.ser.write()
+		self.ser.write(255)
+		self.ser.write(85)
+		self.ser.write(170)
+		self.ser.write(119)
+		self.ser.write(0)
+		self.ser.write(104)
+		self.ser.write(221)
 
 	def get_robotPose(self):
 		if (self.ser.in_waiting() != 0):
 			self.recBuf.append(ser.read())
 
-			if ( len(self.recBuf) > 3 and len(self.recBuf) < 28 ):
-				self.recChk = self.recChk + self.recBuf[len(self.recBuf)-1]%2
+			bufLen = len(self.recBuf)
+			if ( bufLen >= 28 
+				and self.recBuf[bufLen - 28] == 251
+				and self.recBuf[bufLen - 27] == 109
+				and self.recBuf[bufLen - 26] == 37):
+
+				dataOffset = bufLen - 25
+
+				self.recChk = 397
+
+				for i in range (0,6):
+					self.recChk = self.recChk + self.recBuf[dataOffset + 2*i] + self.recBuf[dataOffset + 2*i + 1]
+
+				for i in range (0,6):
+					self.recChk = self.recChk + self.recBuf[dataOffset+12 + 2*i] + self.recBuf[dataOffset+12 + 2*i + 1]
 
 
-			elif ( len(self.recBuf) == 1 ):
-				if ( self.recBuf[0] == 251 ):
-					pass
-				else:
-					self.recBuf = []
-					self.recChk = 0
+				if ( self.recChk == self.recBuf[bufLen-1] ):
+					for i in range (0,6):
+						axisA[i] = self.recBuf[dataOffset + 2*i]*256 + self.recBuf[dataOffset + 2*i + 1]
 
-			elif ( len(self.recBuf) == 2 ):
-				if ( self.recBuf[1] == 109 ):
-					pass
-				else:
-					self.recBuf = []
-					self.recChk = 0
-
-			elif ( len(self.recBuf) == 3 ):
-				if ( self.recBuf[2] == 37 ):
-					pass
-				else:
-					self.recBuf = []
-					self.recChk = 0
-
-			elif ( len(self.recBuf) == 28 ):
-				if ( self.recBuf[27]%2 == self.recChk%2 ):
 
 					for i in range (0,6):
-						axisA[i] = self.recBuf[3+2*i]*256 + self.recBuf[3+2*i+1]
+						axisB[i] = self.recBuf[dataOffset+12 + 2*i]*256 + self.recBuf[dataOffset+12 + 2*i + 1]
 
-					for i in range (0,6):
-						axisB[i] = self.recBuf[15+2*i]*256 + self.recBuf[15+2*i+1]
-
-					pass
 				
 				self.recBuf = []
-				self.recChk = 0
+
 
 
 
