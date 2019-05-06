b72ec8f523f1d46973cc60411fc0a3a379f5a1d9
WUT_DDK_SFB <295054118@qq.com>
2019-05-07 00:01:30 +0800

    验算不稳定，指向很稳定，有跳变

--- ikMount/ikTest.py
+++ ikMount/ikTest.py
@@ -4,7 +4,7 @@
 # @Author: Ruige_Lee
 # @Date:   2019-04-24 19:15:24
 # @Last Modified by:   29505
-# @Last Modified time: 2019-05-06 23:54:34
+# @Last Modified time: 2019-05-07 00:00:29
 # @Email: 295054118@whut.edu.cn
 # @page: https://whutddk.github.io/
 # @File Name: ikTest.py
@@ -142,9 +142,9 @@ if __name__ == "__main__":
 
 	for h in range(0,53):
 		# wristX,wristY,wristZ = ik_find_endCoordinate(0,0,1,0,1,0,-1,0,0,100,100,10*h)
-		# wristX,wristY,wristZ = ik_find_endCoordinate(1,0,0,0,1,0,0,0,1,1,5*h,10*h)
-		theta1,theta2,theta3 = ik_solve_Coordinate(100,0,10*h)
-		theta4,theta5,theta6 = ik_solve_Posture(1,0,0,0,1,0,0,0,1,theta1,theta2,theta3)
+		wristX,wristY,wristZ = ik_find_endCoordinate(-1,0,0,0,-1,0,0,0,-1,100,5*h,10*h)
+		theta1,theta2,theta3 = ik_solve_Coordinate(wristX,wristY,wristZ)
+		theta4,theta5,theta6 = ik_solve_Posture(-1,0,0,0,-1,0,0,0,-1,theta1,theta2,theta3)
 		
 		# obj = ik.objective(link,R=[1,0,0,0,0,1,0,-1,0],t=[0.2,0.2,0.01*h])
 		# solver = ik.solver(obj)
